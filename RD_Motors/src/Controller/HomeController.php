11ab589fb3ccee91cada143d443df867b0349018
Brigode <renaud.brigode@gmail.com>
2023-03-18 17:34:06 +0100

    Change Title

--- RD_Motors/src/Controller/HomeController.php
+++ RD_Motors/src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'title' => 'Accueil',
+            'title' => 'Test 1',
         ]);
     }
 }
